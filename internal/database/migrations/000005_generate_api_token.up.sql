a3960c6e0968961419c906501f4c160f5ae5a558
Anton Prokhorov <russtone@yandex.ru>
2020-06-17 18:58:33 +0300

    fix: Fix telegram.id and api.token migrations (#28)

--- internal/database/migrations/000005_generate_api_token.up.sql
+++ internal/database/migrations/000005_generate_api_token.up.sql
@@ -1,2 +1,2 @@
 CREATE extension IF NOT EXISTS pgcrypto;
-UPDATE users SET params = params || jsonb_build_object('apiToken', encode(gen_random_bytes(16), 'hex')) WHERE NOT params ? 'apiToken' OR params->'apiToken' IS NULL;
+UPDATE users SET params = params || jsonb_build_object('api.token', encode(gen_random_bytes(16), 'hex')) WHERE NOT params ? 'api.token' OR params->'api.token' IS NULL;
