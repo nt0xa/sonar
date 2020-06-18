862056531602ecf7d9588dce6170efa01d285f13
Anton Prokhorov <russtone@yandex.ru>
2020-06-18 15:23:02 +0300

    chore: Create separate db column for admin flag (#30)

---
+++ internal/database/migrations/000008_separate_is_admin_field.up.sql
@@ -0,0 +1,8 @@
+ALTER TABLE users ADD COLUMN is_admin BOOL NOT NULL DEFAULT false;
+
+WITH admins AS (
+    SELECT * FROM users WHERE (params->'admin')::BOOL = true
+)
+UPDATE users SET is_admin = true FROM admins WHERE users.id = admins.id;
+
+UPDATE users SET params = params #- '{admin}';
