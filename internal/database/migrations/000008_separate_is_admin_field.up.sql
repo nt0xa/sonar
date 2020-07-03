57ed4e3af4e0e2094a70fec74324dc73a8e234aa
russtone <russtone@yandex.ru>
2020-07-03 17:55:48 +0300

    fix: Fix migrations

--- internal/database/migrations/000008_separate_is_admin_field.up.sql
+++ internal/database/migrations/000008_separate_is_admin_field.up.sql
@@ -1,7 +1,7 @@
 ALTER TABLE users ADD COLUMN is_admin BOOL NOT NULL DEFAULT false;
 
 WITH admins AS (
-    SELECT * FROM users WHERE (params->'admin')::BOOL = true
+    SELECT * FROM users WHERE (params->>'admin')::BOOL = true
 )
 UPDATE users SET is_admin = true FROM admins WHERE users.id = admins.id;
 
