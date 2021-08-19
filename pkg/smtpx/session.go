fe12fd9bf26f7062c5c8a103315f88ebaddac38d
Anton Prokhorov <russtone@yandex.ru>
2021-08-19 15:57:10 +0300

    refactor: Refactor smtpx package (#97)

--- pkg/smtpx/session.go
+++ pkg/smtpx/session.go
@@ -11,10 +11,10 @@ import (
 	"net"
 	"regexp"
 	"strings"
-	"sync"
 	"time"
 )
 
+// SMTP session states.
 const (
 	stateHelo = iota
 	stateMailFrom
@@ -27,71 +27,114 @@ var (
 	ErrQuit    = errors.New("connection closed")
 )
 
+// Data stores args passed to the corresponding SMTP commands.
 type Data struct {
-	Helo     string
-	Ehlo     string
+	// Helo is a "HELO" command data.
+	Helo string
+
+	// Ehlo is a "EHLO" command data.
+	Ehlo string
+
+	// MailFrom is a "MAIL FROM" command data.
 	MailFrom string
-	RcptTo   []string
-	Data     string
+
+	// RcptTo is a "RCPT TO" command data.
+	RcptTo []string
+
+	// Data is a "DATA" command data.
+	Data string
 }
 
+// Event represents SMTP event.
 type Event struct {
+	// RemoteAddre is remote IP address.
 	RemoteAddr net.Addr
-	Log        []byte
-	Data       *Data
-	Secure     bool
+
+	// Log is a full session log.
+	Log []byte
+
+	// Data stores args passed to the corresponding SMTP commands during
+	// a session.
+	Data *Data
+
+	// Secure shows connection was secure (with TLS).
+	Secure bool
+
+	// ReceivedAt is a session start time.
 	ReceivedAt time.Time
 }
 
-type Session struct {
-	domain    string
-	tlsConfig *tls.Config
+// Msg is used to store provided command responses.
+type Msg struct {
+	// Greet is server greet message.
+	Greet string
 
-	onClose func(*Event)
-
-	lines   chan string
-	rdy     chan struct{}
-	conn    net.Conn
-	r       *bufio.Reader
-	w       *bufio.Writer
-	scanner *bufio.Scanner
-	conv    *bytes.Buffer
-
-	state int
-
-	msg *Data
-
-	mu sync.RWMutex
+	// Ehlo is a first line of EHLO command response.
+	Ehlo string
 }
 
-func NewSession(conn net.Conn, domain string, tlsConfig *tls.Config, onClose func(*Event)) *Session {
+// session contains all data required to handle SMTP session.
+type session struct {
+	// messages stores provided command responses.
+	messages Msg
+
+	// tlsConfig is an optional TLS config.
+	// Required to handle STARTTLS command.
+	tlsConfig *tls.Config
+
+	// onClose is a function that will be called when session is ended.
+	onClose func(*Event)
+
+	// conn is a current TCP connection.
+	conn net.Conn
+
+	// r is a connection reader.
+	r *bufio.Reader
+
+	// w is a connection writer.
+	w *bufio.Writer
+
+	// scanner is a connection reader scanner.
+	scanner *bufio.Scanner
+
+	// rw is a session log.
+	log *bytes.Buffer
+
+	// state is a current state of session.
+	state int
+
+	// data stores args passed to the corresponding SMTP commands during
+	// a session.
+	data *Data
+}
+
+// handleConn creates new SMTP session and handles connection with it.
+func handleConn(ctx context.Context, conn net.Conn, opts options) error {
 	var buf bytes.Buffer
 
 	r := bufio.NewReader(io.TeeReader(conn, &buf))
 	w := bufio.NewWriter(io.MultiWriter(conn, &buf))
 	scanner := bufio.NewScanner(r)
 
-	return &Session{
-		domain:    domain,
-		tlsConfig: tlsConfig,
-		onClose:   onClose,
-
-		lines:   make(chan string, 1),
-		rdy:     make(chan struct{}, 1),
-		conn:    conn,
-		r:       r,
-		w:       w,
-		scanner: scanner,
-		conv:    &buf,
-
-		state: stateHelo,
-		msg: &Data{
+	sess := &session{
+		messages:  opts.messages,
+		tlsConfig: opts.tlsConfig,
+		onClose:   opts.onClose,
+		conn:      conn,
+		r:         r,
+		w:         w,
+		scanner:   scanner,
+		log:       &buf,
+		state:     stateHelo,
+		data: &Data{
 			RcptTo: make([]string, 0),
 		},
 	}
+
+	return sess.start(ctx)
 }
 
-func (s *Session) start(ctx context.Context) error {
+func (s *session) start(ctx context.Context) error {
 	start := time.Now()
 
 	if s.onClose != nil {
@@ -100,8 +143,8 @@ func (s *Session) start(ctx context.Context) error {
 
 			s.onClose(&Event{
 				RemoteAddr: s.conn.RemoteAddr(),
-				Log:        s.conv.Bytes(),
-				Data:       s.msg,
+				Log:        s.log.Bytes(),
+				Data:       s.data,
 				Secure:     secure,
 				ReceivedAt: start,
 			})
@@ -112,27 +155,18 @@ func (s *Session) start(ctx context.Context) error {
 		return err
 	}
 
-	defer close(s.lines)
-
-	s.ready()
-
 	for {
 		select {
-
 		case <-ctx.Done():
 			return nil
 
-		case <-s.rdy:
-			s.mu.RLock()
+		default:
 			if !s.scanner.Scan() {
-				s.mu.RUnlock()
 				return s.scanner.Err()
 			}
-			s.mu.RUnlock()
 
-			s.lines <- s.scanner.Text()
+			line := s.scanner.Text()
 
-		case line := <-s.lines:
 			if err := s.handle(line); err == ErrQuit {
 				return nil
 			} else if err != nil {
@@ -142,8 +176,7 @@ func (s *Session) start(ctx context.Context) error {
 	}
 }
 
-func (s *Session) handle(line string) error {
-	defer s.ready()
+func (s *session) handle(line string) error {
 
 	cmd, args := s.parseCmd(line)
 
@@ -199,7 +232,11 @@ func (s *Session) handle(line string) error {
 	return nil
 }
 
-func (s *Session) parseCmd(line string) (string, string) {
+//
+// Helpers
+//
+
+func (s *session) parseCmd(line string) (string, string) {
 	parts := strings.SplitN(line, " ", 2)
 
 	cmd := strings.ToUpper(parts[0])
@@ -212,57 +249,68 @@ func (s *Session) parseCmd(line string) (string, string) {
 	return cmd, args
 }
 
-func (s *Session) writeLine(line string) error {
+func (s *session) writeLine(line string) error {
 	if _, err := s.w.WriteString(line + "\r\n"); err != nil {
 		return err
 	}
 	return s.w.Flush()
 }
 
-func (s *Session) badSequenceError() error {
+func (s *session) badSequenceError() error {
 	return s.writeLine("503 Bad sequence of commands")
 }
 
-func (s *Session) greet() error {
-	return s.writeLine(fmt.Sprintf("220 %s SMTP Server ready", s.domain))
+func (s *session) commandError() error {
+	return s.writeLine("502 Command is not implemented")
 }
 
-func (s *Session) ready() {
-	s.rdy <- struct{}{}
+func (s *session) greet() error {
+	return s.writeLine(fmt.Sprintf("220 %s SMTP Server ready", s.messages.Greet))
 }
 
-func (s *Session) handleHelo(args string) error {
-	s.msg.Helo = args
+//
+// Commands handlers
+//
+
+// HELO
+func (s *session) handleHelo(args string) error {
+	s.data.Helo = args
 	s.state = stateMailFrom
 	return s.writeLine("250 Hello")
 }
 
-func (s *Session) handleNoop(args string) error {
+// NOOP
+func (s *session) handleNoop(args string) error {
 	return s.writeLine("250 OK")
 }
 
-func (s *Session) handleEhlo(args string) error {
-	s.msg.Ehlo = args
+// EHLO
+func (s *session) handleEhlo(args string) error {
+	s.data.Ehlo = args
 	s.state = stateMailFrom
 
-	if s.tlsConfig == nil {
-		return s.writeLine(fmt.Sprintf("250 %s", s.domain))
-	}
-
-	if err := s.writeLine(fmt.Sprintf("250-%s", s.domain)); err != nil {
+	if err := s.writeLine(fmt.Sprintf("250-%s", s.messages.Ehlo)); err != nil {
 		return err
 	}
 
-	return s.writeLine("250 STARTTLS")
+	if s.tlsConfig == nil {
+		return s.writeLine("250-STARTTLS")
+	}
+
+	return s.writeLine("250 HELO")
 }
 
-func (s *Session) handleStartTLS(args string) error {
+// STARTTLS
+func (s *session) handleStartTLS(args string) error {
+
+	if s.tlsConfig == nil {
+		s.writeLine("502 Command is not implemented")
+		return nil
+	}
+
 	if err := s.writeLine("220 Ready to start TLS"); err != nil {
 		return err
 	}
-
-	s.mu.Lock()
-	defer s.mu.Unlock()
 
 	conn := tls.Server(s.conn, s.tlsConfig)
 
@@ -273,37 +321,40 @@ func (s *Session) handleStartTLS(args string) error {
 	s.conn = net.Conn(conn)
 
 	var buf bytes.Buffer
-	if _, err := io.Copy(&buf, s.conv); err != nil {
+	if _, err := io.Copy(&buf, s.log); err != nil {
 		return err
 	}
 
 	s.r = bufio.NewReader(io.TeeReader(conn, &buf))
 	s.w = bufio.NewWriter(io.MultiWriter(conn, &buf))
 	s.scanner = bufio.NewScanner(s.r)
-	s.conv = &buf
+	s.log = &buf
 	s.state = stateHelo
 
 	return nil
 }
 
-func (s *Session) handleMailFrom(args string) error {
-	s.msg.MailFrom = addrRegexp.ReplaceAllString(args, "$2")
+// MAIL FROM
+func (s *session) handleMailFrom(args string) error {
+	s.data.MailFrom = addrRegexp.ReplaceAllString(args, "$2")
 	s.state = stateRcptTo
 	return s.writeLine("250 OK")
 }
 
-func (s *Session) handleRcptTo(args string) error {
-	s.msg.RcptTo = append(s.msg.RcptTo, addrRegexp.ReplaceAllString(args, "$2"))
+// RCPT TO
+func (s *session) handleRcptTo(args string) error {
+	s.data.RcptTo = append(s.data.RcptTo, addrRegexp.ReplaceAllString(args, "$2"))
 	s.state = stateRcptTo
 	return s.writeLine("250 OK")
 }
 
-func (s *Session) handleData(args string) error {
+// DATA
+func (s *session) handleData(args string) error {
 	if args == "" && s.state != stateData {
 		s.state = stateData
 		return s.writeLine("354 Send data")
 	} else if args != "." {
-		s.msg.Data += args + "\n"
+		s.data.Data += args + "\n"
 		return nil
 	}
 
@@ -311,7 +362,8 @@ func (s *Session) handleData(args string) error {
 	return s.writeLine("250 OK")
 }
 
-func (s *Session) handleQuit(args string) error {
+// QUIT
+func (s *session) handleQuit(args string) error {
 	_ = s.writeLine("221 OK")
 	return ErrQuit
 }
