bc9f9f378904dc586fd7c0361d82957eb2e09ab9
Anton Prokhorov <russtone@yandex.ru>
2023-02-17 12:13:53 +0000

    Implement client config (#121)

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"context"
-	"net/url"
 	"os"
 
+	"github.com/adrg/xdg"
 	"github.com/gookit/color"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
 	"github.com/russtone/sonar/internal/actions"
 	"github.com/russtone/sonar/internal/cmd"
 	"github.com/russtone/sonar/internal/modules/api/apiclient"
 	"github.com/russtone/sonar/internal/utils/slice"
-	"github.com/spf13/cobra"
 )
 
 func main() {
@@ -29,41 +31,39 @@ func main() {
 	}
 
 	//
-	// URL & token
+	// Config
 	//
 
-	var (
-		baseURL, token string
-		insecure       bool
-		proxy          *string
-	)
+	var cfg Config
 
-	if baseURL = os.Getenv("SONAR_URL"); baseURL == "" {
-		fatal("Empty SONAR_URL")
-	}
-
-	u, err := url.Parse(baseURL)
+	configFilePath, err := xdg.ConfigFile("sonar/config.toml")
 	if err != nil {
-		fatal(err)
+		fatalf("Fail to obtain config file path: %v", err)
+	}
+	viper.SetConfigFile(configFilePath)
+
+	if err := viper.ReadInConfig(); err != nil {
+		fatalf("Fail to read config: %v", err)
 	}
 
-	if token = os.Getenv("SONAR_TOKEN"); token == "" {
-		fatal("Empty SONAR_TOKEN")
+	if err := viper.Unmarshal(&cfg); err != nil {
+		fatalf("Fail to unmarshal config: %v", err)
 	}
 
-	if os.Getenv("SONAR_INSECURE") != "" {
-		insecure = true
+	if err := cfg.ValidateWithContext(context.Background()); err != nil {
+		fatalf("Config validation failed: %v", err)
 	}
 
-	if p := os.Getenv("SONAR_PROXY"); p != "" {
-		proxy = &p
+	srv, ok := cfg.Servers[cfg.Context.Server]
+	if !ok {
+		fatalf("Invalid server %q", cfg.Context.Server)
 	}
 
 	//
 	// API client
 	//
 
-	client := apiclient.New(baseURL, token, insecure, proxy)
+	client := apiclient.New(srv.URL, srv.Token, srv.Insecure, srv.Proxy)
 
 	//
 	// User
@@ -84,14 +84,14 @@ func main() {
 	if slice.StringsContains(os.Args, "--json") {
 		handler = &jsonHandler{os.Stdout}
 	} else {
-		handler = &terminalHandler{u.Hostname()}
+		handler = &terminalHandler{srv.BaseURL().Hostname()}
 	}
 
 	root := cmd.New(client, handler, nil).Root(user, true)
 	addJSONFlag(root)
+	addContextCmd(&cfg, root)
 	root.SilenceErrors = true
 	root.SilenceUsage = true
-
 
 	if err := root.Execute(); err != nil {
 		fatal(err)
@@ -112,10 +112,44 @@ func addJSONFlag(root *cobra.Command) {
 
 		cmd.Flags().BoolVar(&jsonOutput, "json", false, "JSON output")
 	}
+}
 
+func addContextCmd(cfg *Config, root *cobra.Command) {
+	var server string
+
+	cmd := &cobra.Command{
+		Use:   "ctx",
+		Short: "Change current context parameters",
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			if err := viper.Unmarshal(&cfg); err != nil {
+				fatalf("Fail to unmarshal config: %v", err)
+			}
+
+			if err := cfg.ValidateWithContext(context.Background()); err != nil {
+				fatalf("Config validation failed: %v", err)
+			}
+
+			if err := viper.WriteConfig(); err != nil {
+				fatalf("Fail to update config: %v", err)
+			}
+
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVarP(&server, "server", "s", "", "Server name from list of servers")
+	viper.BindPFlag("context.server", cmd.Flags().Lookup("server"))
+
+	root.AddCommand(cmd)
 }
 
 func fatal(data interface{}) {
 	color.Danger.Println(data)
+	os.Exit(1)
+}
+
+func fatalf(format string, a ...interface{}) {
+	color.Danger.Printf(format+"\n", a...)
 	os.Exit(1)
 }
