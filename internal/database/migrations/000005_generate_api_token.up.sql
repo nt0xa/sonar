9b476b0f5a86db457120b80291e91f97410a4254
Anton Prokhorov <russtone@yandex.ru>
2024-05-15 10:18:05 +0100

    Remove "if exists" from migrations (#165)

--- internal/database/migrations/000005_generate_api_token.up.sql
+++ internal/database/migrations/000005_generate_api_token.up.sql
@@ -1,2 +1,2 @@
-CREATE extension IF NOT EXISTS pgcrypto;
+CREATE extension pgcrypto;
 UPDATE users SET params = params || jsonb_build_object('api.token', encode(gen_random_bytes(16), 'hex')) WHERE NOT params ? 'api.token' OR params->'api.token' IS NULL;
