d139ce8641a6b4f82b40d7b581eb06191f0b8b80
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2024-07-28 12:27:56 +0100

    Setup CI for deployment

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -7,22 +7,15 @@ const config: Config = {
   tagline: "Dinosaurs are cool",
   favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  url: "https://nt0xa.github.io",
+  baseUrl: "/sonar/",
 
-  // GitHub pages deployment config.
   organizationName: "nt0xa",
   projectName: "sonar",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: "en",
     locales: ["en"],
@@ -35,8 +28,6 @@ const config: Config = {
         docs: {
           routeBasePath: "/",
           sidebarPath: "./sidebars.ts",
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
           editUrl: "https://github.com/nt0xa/sonar/tree/master/docs/",
         },
         blog: false,
