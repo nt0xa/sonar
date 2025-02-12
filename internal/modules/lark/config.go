8e686be12f93cc5d26a2a0ab00106814e17cdaaf
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2025-02-12 15:19:37 +0000

    Added support for Lark persistent websocket connection (#185)

--- internal/modules/lark/config.go
+++ internal/modules/lark/config.go
@@ -9,18 +9,25 @@ type Config struct {
 	AppID             string `mapstructure:"app_id"`
 	AppSecret         string `mapstructure:"app_secret"`
 	VerificationToken string `mapstructure:"verification_token"`
+	Mode              string `mapstructure:"mode"`
 	EncryptKey        string `mapstructure:"encrypt_key"`
 	TLSEnabled        bool   `mapstructure:"tls_enabled"`
 	ProxyURL          string `mapstructure:"proxy_url"`
 	ProxyInsecure     bool   `mapstructure:"proxy_insecure"`
 }
 
+const (
+	ModeWebhook   = "webhook"
+	ModeWebsocket = "websocket"
+)
+
 func (c Config) Validate() error {
 	return validation.ValidateStruct(&c,
 		validation.Field(&c.Admin, validation.Required),
 		validation.Field(&c.AppID, validation.Required),
 		validation.Field(&c.AppSecret, validation.Required),
-		validation.Field(&c.VerificationToken, validation.Required),
-		validation.Field(&c.EncryptKey),
+		validation.Field(&c.Mode, validation.In(ModeWebhook, ModeWebsocket)),
+		validation.Field(&c.VerificationToken, validation.When(c.Mode == ModeWebhook, validation.Required)),
+		validation.Field(&c.EncryptKey, validation.When(c.Mode == ModeWebhook, validation.Required)),
 	)
 }
