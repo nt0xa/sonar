28dab25ccc1f43930e47e5ddf83a292f19b8e1b4
Anton Prokhorov <prokhorov.anton.s@gmail.com>
2025-07-11 22:03:55 +0100

    Initial telemetry implementation

--- pkg/telemetry/telemetry.go
+++ pkg/telemetry/telemetry.go
@@ -3,26 +3,38 @@ package telemetry
 import (
 	"context"
 	"fmt"
+	"log/slog"
 	"os"
 
+	"go.opentelemetry.io/contrib/bridges/otelslog"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	otelmetric "go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/sdk/log"
 	"go.opentelemetry.io/otel/sdk/metric"
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.34.0"
+	oteltrace "go.opentelemetry.io/otel/trace"
 )
 
-type Telemetry struct {
-	lp *log.LoggerProvider
-	mp *metric.MeterProvider
-	tp *trace.TracerProvider
+type telemetry struct {
+	lp     *log.LoggerProvider
+	mp     *metric.MeterProvider
+	tp     *trace.TracerProvider
+	meter  otelmetric.Meter
+	tracer oteltrace.Tracer
 }
 
-func New(ctx context.Context, name, version string) (*Telemetry, error) {
+type Telemetry interface {
+	TraceStart(ctx context.Context, name string) (context.Context, oteltrace.Span)
+	NewLogger(name string) *slog.Logger
+	Shutdown(ctx context.Context) error
+}
+
+func New(ctx context.Context, name, version string) (Telemetry, error) {
 	res := newResource(name, version)
 
 	lp, err := newLoggerProvider(ctx, res)
@@ -34,20 +46,24 @@ func New(ctx context.Context, name, version string) (*Telemetry, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to create metric provider: %w", err)
 	}
+	meter := mp.Meter(name)
 
 	tp, err := newTracerProvider(ctx, res)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create tracer provider: %w", err)
 	}
+	tracer := tp.Tracer(name)
 
-	return &Telemetry{
-		lp: lp,
-		mp: mp,
-		tp: tp,
+	return &telemetry{
+		lp:     lp,
+		mp:     mp,
+		tp:     tp,
+		meter:  meter,
+		tracer: tracer,
 	}, nil
 }
 
-func (t *Telemetry) Shutdown(ctx context.Context) error {
+func (t *telemetry) Shutdown(ctx context.Context) error {
 	if err := t.lp.Shutdown(ctx); err != nil {
 		return err
 	}
@@ -61,6 +77,15 @@ func (t *Telemetry) Shutdown(ctx context.Context) error {
 	}
 
 	return nil
+}
+
+func (t *telemetry) TraceStart(ctx context.Context, name string) (context.Context, oteltrace.Span) {
+	//nolint: spancheck // this is a wrapper around the otel tracer
+	return t.tracer.Start(ctx, name)
+}
+
+func (t *telemetry) NewLogger(name string) *slog.Logger {
+	return otelslog.NewLogger(name, otelslog.WithLoggerProvider(t.lp))
 }
 
 func newResource(name, version string) *resource.Resource {
