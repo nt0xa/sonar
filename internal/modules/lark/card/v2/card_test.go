1dd8c112984f4ae4a732a50719a28b7f3a003910
Anton Prokhorov <anton.prokhorov@bytedance.com>
2025-08-05 14:41:34 +0100

    New Lark notifications with geoip data
    Better looking Lark notifications with new message cards v2: https://open.feishu.cn/cardkit

---
+++ internal/modules/lark/card/v2/card_test.go
@@ -0,0 +1,206 @@
+package card_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/nt0xa/sonar/internal/database/models"
+	"github.com/nt0xa/sonar/internal/modules"
+	"github.com/nt0xa/sonar/internal/modules/lark/card/v2"
+	"github.com/stretchr/testify/require"
+	"gopkg.in/square/go-jose.v2/json"
+)
+
+var expectedJSON = `{
+    "schema": "2.0",
+    "config": {
+        "update_multi": true,
+        "locales": [
+            "default"
+        ],
+        "style": {
+            "text_size": {
+                "custom": {
+                    "default": "normal",
+                    "pc": "normal",
+                    "mobile": "normal"
+                }
+            }
+        }
+    },
+    "body": {
+        "direction": "vertical",
+        "padding": "12px 12px 12px 12px",
+        "elements": [
+            {
+                "tag": "column_set",
+                "horizontal_spacing": "8px",
+                "horizontal_align": "left",
+                "columns": [
+                    {
+                        "tag": "column",
+                        "width": "weighted",
+                        "elements": [
+                            {
+                                "tag": "markdown",
+                                "content": "<font color=\"grey\">IP</font>\n10.13.37.1",
+                                "text_align": "left",
+                                "text_size": "custom",
+                                "margin": "0px 0px 0px 0px",
+                                "icon": {
+                                    "tag": "standard_icon",
+                                    "token": "lan_outlined",
+                                    "color": "green"
+                                }
+                            }
+                        ],
+                        "vertical_spacing": "8px",
+                        "horizontal_align": "left",
+                        "vertical_align": "top",
+                        "weight": 1
+                    },
+                    {
+                        "tag": "column",
+                        "width": "weighted",
+                        "elements": [
+                            {
+                                "tag": "markdown",
+                                "content": "<font color=\"grey\">Time</font>\n01 Jan 2023 00:00:00 UTC",
+                                "text_align": "left",
+                                "text_size": "custom",
+                                "margin": "0px 0px 0px 0px",
+                                "icon": {
+                                    "tag": "standard_icon",
+                                    "token": "time_outlined",
+                                    "color": "blue"
+                                }
+                            }
+                        ],
+                        "vertical_spacing": "8px",
+                        "horizontal_align": "left",
+                        "vertical_align": "top",
+                        "weight": 1
+                    }
+                ],
+                "margin": "0px 0px 0px 0px"
+            },
+            {
+                "tag": "column_set",
+                "horizontal_spacing": "8px",
+                "horizontal_align": "left",
+                "columns": [
+                    {
+                        "tag": "column",
+                        "width": "weighted",
+                        "elements": [
+                            {
+                                "tag": "markdown",
+                                "content": "<font color=\"grey\">Location</font>\n🇬🇧 United Kingdom, London",
+                                "text_align": "left",
+                                "text_size": "custom",
+                                "margin": "0px 0px 0px 0px",
+                                "icon": {
+                                    "tag": "standard_icon",
+                                    "token": "local_outlined",
+                                    "color": "red"
+                                }
+                            }
+                        ],
+                        "vertical_spacing": "8px",
+                        "horizontal_align": "left",
+                        "vertical_align": "top",
+                        "weight": 1
+                    },
+                    {
+                        "tag": "column",
+                        "width": "weighted",
+                        "elements": [
+                            {
+                                "tag": "markdown",
+                                "content": "<font color=\"grey\">Org</font>\nGoogle Inc. (AS1234)",
+                                "text_align": "left",
+                                "text_size": "custom",
+                                "margin": "0px 0px 0px 0px",
+                                "icon": {
+                                    "tag": "standard_icon",
+                                    "token": "company_outlined",
+                                    "color": "orange"
+                                }
+                            }
+                        ],
+                        "vertical_spacing": "8px",
+                        "horizontal_align": "left",
+                        "vertical_align": "top",
+                        "weight": 1
+                    }
+                ],
+                "margin": "0px 0px 0px 0px"
+            },
+            {
+                "tag": "hr",
+                "margin": "0px 0px 0px 0px"
+            },
+            {
+                "tag": "markdown",
+                "content": "` + "```\\ntest\\n```" + `",
+                "text_align": "left",
+                "text_size": "custom",
+                "margin": "0px 0px 0px 0px"
+            }
+        ]
+    },
+    "header": {
+        "title": {
+            "tag": "plain_text",
+            "content": "[test] HTTP"
+        },
+        "subtitle": {
+            "tag": "plain_text",
+            "content": ""
+        },
+        "template": "wathet",
+        "icon": {
+            "tag": "standard_icon",
+            "token": "language_filled"
+        },
+        "padding": "12px 12px 12px 12px"
+    }
+}`
+
+func TestCard(t *testing.T) {
+	receivedAt, _ := time.Parse("2006-01-02T15:04:05Z", "2023-01-01T00:00:00Z")
+
+	card, err := card.Build(&modules.Notification{
+		User:    &models.User{},
+		Payload: &models.Payload{Name: "test"},
+		Event: &models.Event{
+			Protocol: models.Proto{Name: "http"},
+			RW:       []byte("test"),
+			Meta: models.Meta{
+				"geoip": map[string]any{
+					"city": "London",
+					"country": map[string]any{
+						"name":      "United Kingdom",
+						"isoCode":   "GB",
+						"flagEmoji": "🇬🇧",
+					},
+					"asn": map[string]any{
+						"org":    "Google Inc.",
+						"number": 1234,
+					},
+				},
+			},
+			RemoteAddr: "10.13.37.1:1337",
+			ReceivedAt: receivedAt,
+		},
+	})
+
+	require.NoError(t, err)
+
+	var expected, got any
+
+	require.NoError(t, json.Unmarshal([]byte(expectedJSON), &expected))
+	require.NoError(t, json.Unmarshal(card, &got))
+
+	require.Equal(t, expected, got)
+}
